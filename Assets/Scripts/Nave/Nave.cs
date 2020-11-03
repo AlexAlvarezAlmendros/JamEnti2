ea755b412e87f2c31aee1bd4e10ef3502b5ca129
victorenti <56065454+victorenti@users.noreply.github.com>
2020-11-03 09:13:57 +0100

    nave

---
+++ Assets/Scripts/Nave/Nave.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Nave : MonoBehaviour
+{
+    public float velXY = 10;
+    public float velZ = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            velZ = 18;
+        }
+
+        else
+        {
+            velZ = 5;
+        }
+        //transform.position = new Vector3(transform.position.x + (Input.GetAxis("Horizontal") * velXY *Time.deltaTime * direction), transform.position.y + (Input.GetAxis("Vertical") * velXY *Time.deltaTime), transform.position.z +( velZ * Time.deltaTime * direction));
+        transform.position = transform.position + (transform.forward * velZ * Time.deltaTime);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x + (Input.GetAxis("Vertical") * Time.deltaTime *100), transform.eulerAngles.y + (Input.GetAxis("Horizontal") * Time.deltaTime *100), transform.eulerAngles.z);
+    }
+}
