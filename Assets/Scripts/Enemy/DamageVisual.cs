cb7cbe513e3eb2495022b47d0924f2e74c97064f
AlexAlvarezAlmendros <alexalmendrosal@gmail.com>
2020-11-04 04:46:22 +0100

    Enemy in main

---
+++ Assets/Scripts/Enemy/DamageVisual.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DamageVisual : MonoBehaviour
+{
+    public EnemyController controller;
+    public GameObject explosion;
+
+    public void OnDamage()
+    {
+        if (controller.health < 0f) {
+            var explosion_ = Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(explosion_.gameObject, 1f);
+
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            OnDamage();
+            Debug.Log("Damage");
+            Destroy(col.gameObject);
+        }
+    }
+}
